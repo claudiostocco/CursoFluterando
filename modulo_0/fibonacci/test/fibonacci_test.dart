5b2a568fd9560c9746e889bf5a78c1c6d38fb908
claudiostocco <claudiostocco@gmail.com>
2024-06-27 18:01:15 -0300

    Implements tests

--- modulo_0/fibonacci/test/fibonacci_test.dart
+++ modulo_0/fibonacci/test/fibonacci_test.dart
@@ -3,6 +3,6 @@ import 'package:test/test.dart';
 
 void main() {
   test('calculate', () {
-    expect(calculate(), 42);
+    expect(FibonacciImpl().generate(), [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]);
   });
 }
--- modulo_0/imc-calculate/test/imc_calculate_test.dart
+++ modulo_0/imc-calculate/test/imc_calculate_test.dart
@@ -1,8 +1,18 @@
 import 'package:imc_calculate/imc_calculate.dart';
+import 'package:imc_calculate/imc_calculate_impl.dart';
 import 'package:test/test.dart';
 
 void main() {
+  ImcCalculate instance = ImcCalculateImpl('Claudio');
+
   test('calculate', () {
-    expect(calculate(), 42);
+    expect(instance.calculate(weight: 80, height: 1.73), 26.73);
+  });
+
+  test('analyse', () {
+    var imc = instance.calculate(weight: 80, height: 1.73);
+    var result = 'Atenção! Você está com sobrepeso.';
+    var name = 'Claudio';
+    expect(instance.analyse(imc: imc), '$name\n$result');
   });
 }
